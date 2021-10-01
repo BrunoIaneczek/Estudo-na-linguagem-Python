4480b943ccc3ceebd55eaf512ed5bc549c2c8fa8
BrunoIaneczek / BrunoIaneczek <brunoamaralianeczek@gmailcom>
2021-09-30 21:22:53 -0300

    Update Exercício_01.py

--- Python_Brasil_exercicios/Estrutura_de_Repetição/Exercício_01.py
+++ Python_Brasil_exercicios/Estrutura_de_Repetição/Exercício_01.py
@@ -4,7 +4,7 @@ Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o us
 while True:
     nota = float(input('Digite uma nota entre 0 e 10: '))
     while nota < 0 or nota > 10:
-        nota = float(input('Valor inválido, digite outro valor: '))
+        nota = float(input('Valor inválido , digite outro valor: '))
     print('Nota armazenada com sucesso')
     break
 
